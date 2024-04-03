c10e68174ca1005b0973ada7ca3cb9398c4a27a8
Timothee LAPOV <artemlapov@me.com>
2024-04-03 13:17:06 +0200

    Initial Commit

---
+++ CartesDeMots/CartesDeMotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartesDeMotsApp.swift
+//  CartesDeMots
+//
+//  Created by Timothée LAPOV on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CartesDeMotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CartesDeMots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CartesDeMots
+//
+//  Created by Timothée LAPOV on 03/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
