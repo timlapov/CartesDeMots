99f425905c5367cf549addcab856e4a204a90af8
Timothee LAPOV <artemlapov@me.com>
2024-07-04 16:15:03 +0200

    Haptic feedback integrated into the app

---
+++ CartesDeMots/Model/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  CartesDeMots
+//
+//  Created by Timothée LAPOV on 03/07/2024.
+//
+
+import Foundation
---
+++ CartesDeMots/Utilities/HapticManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticManager.swift
+//  CartesDeMots
+//
+//  Created by Timothée LAPOV on 04/07/2024.
+//
+
+import Foundation
